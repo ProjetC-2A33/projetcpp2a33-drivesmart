60c98f60d1d2b3e878948ce8500d7c3d2dae8516
Rihab <rihab.mahfoudhi@esprit.tn>
2025-10-07 20:45:02 +0100

    initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "condidat/mainwindow.h"
+#include "vehicule/vehicule.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    w.showMaximized();
+    return a.exec();
+}
